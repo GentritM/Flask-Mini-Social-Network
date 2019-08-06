eec12ca95ba9718c4bbb194ef8ff255048981b25
GentritM <genti_mehmeti@outlook.com>
2019-08-06 12:24:10 +0200

    updated

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
@@ -8,7 +9,9 @@ app.config['SECRET_KEY'] = '255890dd847ced350c035706b929657b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 bcrypt = Bcrypt(app)
 
 
